83098774548442e57d3cf1328d70d3861c67a9e5
Georgia Makoudi <gmakoudi@lightbuzz.com>
2018-07-26 22:10:50 -0700

    Added checks in certificate validation and changed response retrieval

--- LightBuzz.Azure/LightBuzz.Azure/LightBuzzHttpsHandler.cs
+++ LightBuzz.Azure/LightBuzz.Azure/LightBuzzHttpsHandler.cs
@@ -45,168 +45,147 @@ using UnityEngine.Networking;
 
 namespace LightBuzz.Azure
 {
-    /// <summary>
-    /// A Unity-ready secure HTTPS handler.
-    /// </summary>
-    public class LightBuzzHttpsHandler : HttpClientHandler
-    {
-        /// <summary>
-        /// The response from the server.
-        /// </summary>
-        private HttpResponseMessage _result = new HttpResponseMessage();
+	/// <summary>
+	/// A Unity-ready secure HTTPS handler.
+	/// </summary>
+	public class LightBuzzHttpsHandler : HttpClientHandler
+	{
+		/// <summary>
+		/// The response from the server.
+		/// </summary>
+		private HttpResponseMessage _result = new HttpResponseMessage();
 
-        /// <summary>
-        /// The authorization token for the request.
-        /// </summary>
-        private string _authorizationToken = string.Empty;
+		/// <summary>
+		/// The authorization token for the request.
+		/// </summary>
+		private string _authorizationToken = string.Empty;
 
-        /// <summary>
-        /// The Content Type header type (default: application/json).
-        /// </summary>
-        public string ContentType { get; set; }
+		/// <summary>
+		/// The Content Type header type (default: application/json).
+		/// </summary>
+		public string ContentType { get; set; }
 
-        /// <summary>
-        /// The ZUMO API version number.
-        /// </summary>
-        public string ZumoApiVersion { get; set; }
+		/// <summary>
+		/// The ZUMO API version number.
+		/// </summary>
+		public string ZumoApiVersion { get; set; }
 
-        /// <summary>
-        /// The encoding of the response message.
-        /// </summary>
-        public Encoding Encoding { get; set; }
+		/// <summary>
+		/// The encoding of the response message.
+		/// </summary>
+		public Encoding Encoding { get; set; }
 
-        /// <summary>
-        /// Creates a new LightBuzz secure HTTPS handler.
-        /// </summary>
-        public LightBuzzHttpsHandler()
-        {
-            AutomaticDecompression = DecompressionMethods.Deflate;
-            ContentType = "application/json";
-            ZumoApiVersion = "2.0.0";
-            Encoding = Encoding.UTF8;
-        }
+		/// <summary>
+		/// Creates a new LightBuzz secure HTTPS handler.
+		/// </summary>
+		public LightBuzzHttpsHandler()
+		{
+			AutomaticDecompression = DecompressionMethods.Deflate;
+			ContentType = "application/json";
+			ZumoApiVersion = "2.0.0";
+			Encoding = Encoding.UTF8;
+		}
 
-        /// <summary>
-        /// A Unity-ready implementation of a secure HTTPS method to send the request.
-        /// </summary>
-        /// <param name="request">The request to send to server.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>The response from the server.</returns>
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            string contentArray = request.Content != null ? await request.Content.ReadAsStringAsync() : null;
+		/// <summary>
+		/// A Unity-ready implementation of a secure HTTPS method to send the request.
+		/// </summary>
+		/// <param name="request">The request to send to server.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>The response from the server.</returns>
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			string contentArray = request.Content != null ? await request.Content.ReadAsStringAsync() : null;
 
-            HttpRequestHeaders headers = request.Headers;
-            IEnumerable<string> auth;
-            if (headers != null && headers.TryGetValues("X-ZUMO-AUTH", out auth))
-            {
-                _authorizationToken = headers.GetValues("X-ZUMO-AUTH").FirstOrDefault();
-            }
+			HttpRequestHeaders headers = request.Headers;
+			IEnumerable<string> auth;
+			if (headers != null && headers.TryGetValues("X-ZUMO-AUTH", out auth))
+			{
+				_authorizationToken = headers.GetValues("X-ZUMO-AUTH").FirstOrDefault();
+			}
 
-            SendHttpWebRequest(request, contentArray);
+			SendHttpWebRequest(request, contentArray);
 
-            return _result;
-        }
+			return _result;
+		}
 
-        /// <summary>
-        /// Sends an Http Web Request.
-        /// </summary>
-        /// <param name="request">The request message.</param>
-        /// <param name="contentArray">The request content.</param>
-        /// <returns></returns>
-        private void SendHttpWebRequest(HttpRequestMessage request, string contentArray)
-        {
-            HttpWebRequest client = (HttpWebRequest)WebRequest.Create(request.RequestUri.AbsoluteUri);
+		/// <summary>
+		/// Sends an Http Web Request.
+		/// </summary>
+		/// <param name="request">The request message.</param>
+		/// <param name="contentArray">The request content.</param>
+		/// <returns></returns>
+		private void SendHttpWebRequest(HttpRequestMessage request, string contentArray)
+		{
+			HttpWebRequest client = (HttpWebRequest)WebRequest.Create(request.RequestUri.AbsoluteUri);
 
-            client.Method = request.Method.ToString();
-            client.KeepAlive = true;
-            client.ContentType = ContentType;
+			client.Method = request.Method.ToString();
+			client.KeepAlive = true;
+			client.ContentType = ContentType;
 
-            if (!WebHeaderCollection.IsRestricted("Content-Type"))
-            {
-                client.Headers.Add("Content-Type", ContentType);
-            }
+			if (!WebHeaderCollection.IsRestricted("Content-Type"))
+			{
+				client.Headers.Add("Content-Type", ContentType);
+			}
 
-            if (!WebHeaderCollection.IsRestricted("ZUMO-API-VERSION"))
-            {
-                client.Headers.Add("ZUMO-API-VERSION", ZumoApiVersion);
-            }
+			if (!WebHeaderCollection.IsRestricted("ZUMO-API-VERSION"))
+			{
+				client.Headers.Add("ZUMO-API-VERSION", ZumoApiVersion);
+			}
 
-            if (!string.IsNullOrEmpty(_authorizationToken))
-            {
-                if (!WebHeaderCollection.IsRestricted("X-ZUMO-AUTH"))
-                {
-                    client.Headers.Add("X-ZUMO-AUTH", _authorizationToken);
-                }
-            }
+			if (!string.IsNullOrEmpty(_authorizationToken))
+			{
+				if (!WebHeaderCollection.IsRestricted("X-ZUMO-AUTH"))
+				{
+					client.Headers.Add("X-ZUMO-AUTH", _authorizationToken);
+				}
+			}
 
 #if !UNITY_WSA
-            ServicePointManager.ServerCertificateValidationCallback = LightBuzzCertificateValidation.CertificateValidationCallback;
+			ServicePointManager.ServerCertificateValidationCallback = LightBuzzCertificateValidation.CertificateValidationCallback;
 #endif
 
-            if (contentArray != null)
-            {
-                using (StreamWriter streamWriter = new StreamWriter(client.GetRequestStream()))
-                {
-                    streamWriter.Write(contentArray);
-                }
-            }
+			if (contentArray != null)
+			{
+				using (StreamWriter streamWriter = new StreamWriter(client.GetRequestStream()))
+				{
+					streamWriter.Write(contentArray);
+				}
+			}
 
-            try
-            {
-                using (HttpWebResponse response = (HttpWebResponse)client.GetResponse())
-                {
-                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                    {
-                        string data = reader.ReadToEnd();
+			try
+			{
+				using (HttpWebResponse response = (HttpWebResponse)client.GetResponse())
+				{
+					using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+					{
+						string data = reader.ReadToEnd();
 
-                        _result.StatusCode = HttpStatusCode.OK;
-                        _result.ReasonPhrase = _result.StatusCode.ToString();
-                        _result.Content = new StringContent(data, Encoding, ContentType);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                GetResultStatusCodeFromException(ex);
-                _result.ReasonPhrase = _result.StatusCode.ToString();
-                _result.Content = new StringContent(ex.ToString(), Encoding.UTF8, ContentType);
-            }
-        }
+						_result.StatusCode = response.StatusCode;
+						_result.ReasonPhrase = _result.StatusCode.ToString();
+						_result.Content = new StringContent(data, Encoding, ContentType);
+					}
+				}
+			}
+			catch (WebException webException)
+			{
+				if (webException.Response == null)
+				{
+					throw webException;
+				}
+				using (HttpWebResponse response = (HttpWebResponse)webException.Response)
+				{
+					using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+					{
+						string data = reader.ReadToEnd();
+						_result.StatusCode = response.StatusCode;
+						_result.ReasonPhrase = _result.StatusCode.ToString();
+						_result.Content = new StringContent(data, Encoding, ContentType);
+					}
+				}
+			}
+		}
 
-        /// <summary>
-        /// Gets the result status code depending on the exception message
-        /// </summary>
-        /// <param name="ex">The exception</param>
-        private void GetResultStatusCodeFromException(Exception ex)
-        {
-            if (ex.Message.Contains("Unauthorized"))
-            {
-                _result.StatusCode = HttpStatusCode.Unauthorized;
-            }
-            else if (ex.Message.Contains("Bad Request"))
-            {
-                _result.StatusCode = HttpStatusCode.BadRequest;
-            }
-            else if (ex.Message.Contains("Task Cancelled"))
-            {
-                _result.StatusCode = HttpStatusCode.RequestTimeout;
-            }
-            else if (ex.Message.Contains("Precondition Failed"))
-            {
-                _result.StatusCode = HttpStatusCode.PreconditionFailed;
-            }
-            else if (ex.Message.Contains("Not Found"))
-            {
-                _result.StatusCode = HttpStatusCode.NotFound;
-            }
-            else
-            {
-                _result.StatusCode = HttpStatusCode.InternalServerError;
-            }
-        }
-
-        /*/// <summary>
+		/*/// <summary>
         /// A Unity-ready implementation of a secure HTTPS method to send the request.
         /// </summary>
         /// <param name="request">The request to send to server.</param>
@@ -234,55 +213,55 @@ namespace LightBuzz.Azure
             return _result;
         }*/
 
-        /// <summary>
-        /// Sends a Unity Web Request.
-        /// </summary>
-        /// <param name="url">The request absolute uri.</param>
-        /// <param name="contentArray">The request content.</param>
-        /// <param name="method">The request method.</param>
-        /// <returns>An IEnumerator with the response text.</returns>
-        IEnumerator SendUnityRequest(string url, byte[] contentArray, string method)
-        {
-            UnityWebRequest uwr = new UnityWebRequest(url, method);
+		/// <summary>
+		/// Sends a Unity Web Request.
+		/// </summary>
+		/// <param name="url">The request absolute uri.</param>
+		/// <param name="contentArray">The request content.</param>
+		/// <param name="method">The request method.</param>
+		/// <returns>An IEnumerator with the response text.</returns>
+		IEnumerator SendUnityRequest(string url, byte[] contentArray, string method)
+		{
+			UnityWebRequest uwr = new UnityWebRequest(url, method);
 
-            if (contentArray != null)
-            {
-                uwr.uploadHandler = new UploadHandlerRaw(contentArray);
-            }
+			if (contentArray != null)
+			{
+				uwr.uploadHandler = new UploadHandlerRaw(contentArray);
+			}
 
-            uwr.downloadHandler = new DownloadHandlerBuffer();
-            uwr.SetRequestHeader("Content-Type", ContentType);
-            uwr.SetRequestHeader("ZUMO-API-VERSION", ZumoApiVersion);
-            if (!string.IsNullOrEmpty(_authorizationToken))
-            {
-                uwr.SetRequestHeader("X-ZUMO-AUTH", _authorizationToken);
-            }
+			uwr.downloadHandler = new DownloadHandlerBuffer();
+			uwr.SetRequestHeader("Content-Type", ContentType);
+			uwr.SetRequestHeader("ZUMO-API-VERSION", ZumoApiVersion);
+			if (!string.IsNullOrEmpty(_authorizationToken))
+			{
+				uwr.SetRequestHeader("X-ZUMO-AUTH", _authorizationToken);
+			}
 
-            // Send the request then wait until it returns.
-            yield return uwr.SendWebRequest();
+			// Send the request then wait until it returns.
+			yield return uwr.SendWebRequest();
 
-            while (!uwr.isDone)
-            {
-                yield return null;
-            }
+			while (!uwr.isDone)
+			{
+				yield return null;
+			}
 
-            if (uwr.isNetworkError || uwr.isHttpError)
-            {
-                Debug.LogWarning("Error while sending: " + uwr.error);
-                _result.StatusCode = (HttpStatusCode)uwr.responseCode;
-                _result.ReasonPhrase = _result.StatusCode.ToString();
-                _result.Content = new StringContent(uwr.downloadHandler.text, Encoding.UTF8, ContentType);
+			if (uwr.isNetworkError || uwr.isHttpError)
+			{
+				Debug.LogWarning("Error while sending: " + uwr.error);
+				_result.StatusCode = (HttpStatusCode)uwr.responseCode;
+				_result.ReasonPhrase = _result.StatusCode.ToString();
+				_result.Content = new StringContent(uwr.downloadHandler.text, Encoding.UTF8, ContentType);
 
-                yield return uwr.error;
-            }
-            else
-            {
-                _result.StatusCode = (HttpStatusCode)uwr.responseCode;
-                _result.ReasonPhrase = _result.StatusCode.ToString();
-                _result.Content = new StringContent(uwr.downloadHandler.text, Encoding.UTF8, ContentType);
+				yield return uwr.error;
+			}
+			else
+			{
+				_result.StatusCode = (HttpStatusCode)uwr.responseCode;
+				_result.ReasonPhrase = _result.StatusCode.ToString();
+				_result.Content = new StringContent(uwr.downloadHandler.text, Encoding.UTF8, ContentType);
 
-                yield return uwr.downloadHandler.text;
-            }
-        }
-    }
+				yield return uwr.downloadHandler.text;
+			}
+		}
+	}
 }
